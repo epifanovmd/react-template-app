ecde3ca78a8cbbd5340e1b034a11ac79a25f1e24
Epifanovmd <epifanovmd@gmail.com>
2019-10-16 14:39:12 +0300

    update project config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,14 +16,6 @@ const webpackConfigDev = {
       },
       ...webpackConfig.baseLoaders.font,
       webpackConfig.baseLoaders.file,
-      {
-        test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
-        loader: require.resolve('url-loader'),
-        options: {
-          limit: 10000,
-          name: 'static/media/[name].[hash:8].[ext]',
-        },
-      },
     ],
   },
   plugins: [

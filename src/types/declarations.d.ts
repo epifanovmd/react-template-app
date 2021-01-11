22985bee9b0e9b99113d4d03104f7997cd00f868
Epifanov <epifanovmd@gmail.com>
2021-01-11 02:35:55 +0200

    add svg declare

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -10,3 +10,10 @@ declare module "*.png" {
   const content: string;
   export default content;
 }
+
+declare module "*.svg" {
+  const content: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string | undefined }
+  >;
+  export default content;
+}

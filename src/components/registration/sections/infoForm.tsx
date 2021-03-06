f3ba8671e45b2128552f24467da592758248615f
Epifanov <epifanovmd@gmail.com>
2021-03-06 15:29:31 +0200

    refactoring

---
+++ src/components/registration/sections/infoForm.tsx
@@ -0,0 +1,78 @@
+import { IForm } from "Common/hooks/useForm";
+import { Input } from "Components/controls/Input";
+import { ISelectItem, Select } from "Components/controls/select/select";
+import React, { FC } from "react";
+
+import EmailIcon from "@/icons/email.svg";
+
+import { IRegistrationForm, IRegistrationFormMeta } from "../registration";
+
+interface IProps extends IForm<IRegistrationForm, IRegistrationFormMeta> {}
+
+const genderItems = [
+  { label: "Мужчина", key: "male", description: "Мужской пол" },
+  { label: "Женщина", key: "female", description: "Женский пол" },
+];
+
+export const InfoForm: FC<IProps> = ({
+  values,
+  errors,
+  handleChange,
+  touchedValues,
+  handleBlur,
+  fieldNames,
+  setFieldValue,
+}) => {
+  const onChangeGender = (value: ISelectItem) => {
+    setFieldValue("gender", value);
+  };
+
+  return (
+    <>
+      <Input
+        label={"Имя"}
+        placeholder={"Имя"}
+        defaultValue={values.firstName}
+        onChange={handleChange}
+        name={fieldNames.firstName}
+        onBlur={handleBlur}
+        touch={touchedValues.firstName}
+        error={errors.firstName}
+      />
+
+      <Input
+        label={"Фамилия"}
+        placeholder={"Фамилия"}
+        defaultValue={values.lastName}
+        onChange={handleChange}
+        name={fieldNames.lastName}
+        onBlur={handleBlur}
+        touch={touchedValues.lastName}
+        error={errors.lastName}
+      />
+
+      <Input
+        icon={<EmailIcon />}
+        label={"Email"}
+        placeholder={"Email"}
+        defaultValue={values.email}
+        onChange={handleChange}
+        name={fieldNames.email}
+        onBlur={handleBlur}
+        touch={touchedValues.email}
+        error={errors.email}
+      />
+
+      <Select
+        name={fieldNames.gender}
+        items={genderItems}
+        value={values.gender}
+        label={"Пол"}
+        placeholder={"Выберете пол"}
+        onChange={onChangeGender}
+        touch={touchedValues.gender}
+        error={errors.gender}
+      />
+    </>
+  );
+};

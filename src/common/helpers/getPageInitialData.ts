bb97a8dde4645eff98fd022dd70c50c1f8132069
epifanov <epifanovmd@gmail.com>
2022-04-15 00:11:50 +0300

    fix error

--- src/common/helpers/getPageInitialData.ts
+++ src/common/helpers/getPageInitialData.ts
@@ -1,5 +1,5 @@
 export const getPageInitialData = (pathName: string) => {
-  if (typeof window !== "undefined" && window.__INITIAL_STATE__[pathName]) {
+  if (typeof window !== "undefined" && window.__INITIAL_STATE__?.[pathName]) {
     return window.__INITIAL_STATE__[pathName];
   }
 };

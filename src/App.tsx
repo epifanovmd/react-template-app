ecee5ca152707877ac1be8b9e932d2c8a9c0649c
epifanov <epifanovmd@gmail.com>
2022-04-29 11:48:32 +0300

    add meta

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,21 @@ import { Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import { Header } from "./components";
 import { Container } from "./components/layouts/container";
+import Helmet from "react-helmet";
 
 const App = () => {
   const { i18n } = useTranslation();
 
   return (
     <Container key={i18n.language}>
+      <Helmet>
+        <meta property="og:title" content="SPA SSR APP Example" />
+        <meta
+          property="og:description"
+          content="React SPA Server Side Rendering application example"
+        />
+        <meta property="og:image" content="https://picsum.photos/200/300" />
+      </Helmet>
       <Header />
       <br />
       <Routes>

c5241688667295754ee1915e638eed60edd79dff
Андрей Епифанов <epifanovmd@gmail.com>
2022-11-05 01:16:50 +0300

    fix get initial data

--- src/common/helpers/getInitialData.ts
+++ src/common/helpers/getInitialData.ts
@@ -1,8 +1,24 @@
 export const initialData: any = {};
 export const getInitialData = (tid: string) => {
-  if (typeof window !== "undefined" && window.__INITIAL_STATE__?.[tid]) {
-    return window.__INITIAL_STATE__[tid];
+  if (typeof window !== "undefined") {
+    const d = window.__INITIAL_STATE__?.[tid];
+
+    if (window.__INITIAL_STATE__) {
+      setTimeout(() => {
+        window.__INITIAL_STATE__[tid] = undefined;
+      }, 1000);
+    }
+
+    return d;
   } else {
-    return initialData?.[tid];
+    const d = initialData?.[tid];
+
+    if (initialData) {
+      setTimeout(() => {
+        initialData[tid] = undefined;
+      }, 1000);
+    }
+
+    return d;
   }
 };
